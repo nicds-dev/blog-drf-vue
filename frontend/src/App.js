87c4b0045904c06b5b6f0c148296df7ed67ef01c
NicDs <nikolasdurango@gmail.com>
2024-01-03 14:32:06 -0500

    added django-cors for local development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class connectionExample extends React.Component {
+  apiUrl = 'http://localhost:8000/api';
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(this.apiUrl);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>Connection Example</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connectionExample;
